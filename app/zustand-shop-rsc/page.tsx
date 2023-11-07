e8ab7c38c7d0cd039f6a8ccfc952bdcd0dd8d37b
Christof Haemmerle <reco@nex9.com>
2023-11-07 10:47:58 -0500

    docs, fix issue

--- app/zustand-shop-rsc/page.tsx
+++ app/zustand-shop-rsc/page.tsx
@@ -4,10 +4,19 @@ import Link from "next/link";
 import { Cart } from "./cart";
 import { Product } from "./product";
 import { Add } from "./add";
+import { useStore } from "./store";
 
 export default function Page() {
+  const data = {
+    name: "iPhone",
+    price: 100,
+  };
+
+  useStore.setState(data);
+
   return (
     <>
+      <StoreInitializer name="Phones" price={100} />
       <section>
         <h1>zustand shop RSC</h1>
         <h2>state management</h2>
@@ -18,7 +27,6 @@ export default function Page() {
           </Link>
         </p>
         <div className="flex items-start min-h-screen gap-2 py-2">
-          <StoreInitializer name="Phones" price={100} />
           <Product
             description="The best phone money can buy."
             image="/iphone.jpg"
@@ -26,6 +34,17 @@ export default function Page() {
           <Add />
           <Cart />
         </div>
+
+        <p>
+          For SCs, we set the state with `useStore.setState(data)`. This data
+          will be availabe on the client!
+          <br />
+          <br />
+          For the CCs We create a StoreInitializer CC which gets the data from
+          the server component passed.
+          <br />
+          This way we can also pick an choose what data the client store has.
+        </p>
       </section>
     </>
   );
